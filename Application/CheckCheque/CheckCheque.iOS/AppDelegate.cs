05468780ed64fd6e66a2936f49039851ddc4ff7c
pRopia <pratik.ropia@gmail.com>
2019-07-28 14:41:33 +0200

    Added an iOS project.

---
+++ Application/CheckCheque/CheckCheque.iOS/AppDelegate.cs
@@ -0,0 +1,20 @@
+﻿using Foundation;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+
+namespace CheckCheque.iOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            Xamarin.Forms.Forms.Init();
+
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(application, launchOptions);
+        }
+    }
+}
+
